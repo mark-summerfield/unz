36c7ea1f12ea709cf6e2f738b4f92bfc2787b1ec
mark-summerfield <mark@qtrac.eu>
2023-01-03 10:34:47 +0000

    cli

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/mark-summerfield/unz
 
 go 1.19
+
+require github.com/mark-summerfield/clip v0.7.0
+
+require (
+	github.com/kopoli/go-terminal-size v0.0.0-20170219200355-5c97524c8b54 // indirect
+	github.com/mark-summerfield/gong v0.7.0 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+)
--- unz.go
+++ unz.go
@@ -6,11 +6,48 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"github.com/mark-summerfield/clip"
+	"log"
 )
 
 //go:embed Version.dat
 var Version string
 
 func main() {
-	fmt.Printf("Hello unz v%s\n", Version)
+	verbose, list, archives := getConfig()
+	for _, archive := range archives {
+		if list {
+			listContents(archive, verbose)
+		} else {
+			unpack(archive, verbose)
+		}
+	}
+}
+
+func getConfig() (bool, bool, []string) {
+	parser := clip.NewParserVersion(Version)
+	parser.LongDesc = `Unpacks each archive (tar or zip). For each archive
+	at most one file or folder is created in the current folder. If the
+	archive contains one file or folder, that file or folder is unpacked
+	into the current folder. If the archive contains more than one member,
+	then a new subfolder is created based on the archive's name, and all the
+	archive's contents are unpacked into the subfolder.`
+	parser.PositionalCount = clip.OneOrMorePositionals
+	_ = parser.SetPositionalVarName("ARCHIVE")
+	verboseOpt := parser.Flag("verbose", "Show actions.")
+	listOpt := parser.Flag("list",
+		"List each archive's contents (don't unpack).")
+	err := parser.Parse()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return verboseOpt.Value(), listOpt.Value(), parser.Positionals
+}
+
+func listContents(archive string, verbose bool) {
+	fmt.Println("listContents", archive, verbose) // TODO
+}
+
+func unpack(archive string, verbose bool) {
+	fmt.Println("unpack", archive, verbose) // TODO
 }
